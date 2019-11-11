65c74f64f745c5b5ba1079fa7f2659650fc959a8
Yofremj <38448774+Yofremj@users.noreply.github.com>
2019-11-11 10:52:20 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var music=new Audio();
-music.src="http://plusradio24.ddns.net:8000/Plusradio24";
+music.src="http://plusradio24.ddns.net:8000/plusradio24";
 music.loop=true;
 music.pause();   
    
