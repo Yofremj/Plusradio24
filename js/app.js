df94fe867c398d70893c514c6e013970daff9194
Yofremj <38448774+Yofremj@users.noreply.github.com>
2020-05-22 23:12:45 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 var music=new Audio("http://plusradio24.ddns.net:8000/plusradio24");
 music.play();   
    
-function myFunction(x) {
+ function myFunction(x) {
             
-  x.classList.toggle("fa-stop-circle");
-  x.classList.add("fa-play-circle");
+   x.classList.toggle("fa-stop-circle");
+   x.classList.add("fa-play-circle");
             
-  if(music.play){
-    music.pause();
-  }else{
-    music.play();    
-  }
-    
-}
+   if (isPlaying) {
+     music.pause()
+   } else {
+     music.play();
+   }
+
+ };
+
+ music.onplaying = function() {
+   isPlaying = true;
+ };
+ music.onpause = function() {
+   isPlaying = false;
+ }
