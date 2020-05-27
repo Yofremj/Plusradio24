8eb35ef00ef5eb1945cedb2e94931c129825a061
Yofremj <38448774+Yofremj@users.noreply.github.com>
2020-05-26 21:24:03 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var music = new Audio("http://plusradio24.ddns.net:8000/plusradio24");
 music.pause();   
    
 function myFunction(x){
-    x.classList.toggle('fa-stop-circle');
+    x.classList.toggle("fa-stop-circle");
 
     if(music.paused){
         music.play();
